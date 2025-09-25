e653ee84900a2daad17c18a3df0f8c425f274a18
adrien-saccaggi <adrien.saccaggi@laplateforme.io>
2025-09-25 14:31:19 +0200

    inscription et connexion

--- connexion.php
+++ connexion.php
@@ -2,9 +2,25 @@
 // Une page contenant un formulaire de connexion salut a tous 
 
 ?>
+<head>
+    <link rel= "stylesheet" href="style_connexion.css">
 
+</head>
 <?php include "structure/header.php"; ?>
 <body>
     <?php include "structure/menu.php"; ?>
+            <div class="connection_container">
+
+    <form class="connexion" method="post" action ="model/traitement-connexion.php">
+        <h1>Connexion</h1>
+        <p class="text">Nom d'utilisateur</p>
+        <input name="login" type="text" placeholder="Nom d'utilisateur">
+        <p class="text">Mot de passe</p>
+        <input name="password" type="password" placeholder="Mot de passe">
+        <input class="btn_connexion" name ="bouton" type="submit" value="Envoyer"> 
+
+    </form>
+    </div>
+</div>
 </body>
 </html>
--- inscription.php
+++ inscription.php
@@ -1,19 +1,31 @@
+
 <?php
 // Une page contenant un formulaire d’inscription
 ?>
 
 <?php include "structure/header.php"; ?>
+<head>
+<link rel="stylesheet" href="style_inscription.css">
+
+</head>
 <body>
     <?php include "structure/menu.php"; ?>
+<<<<<<< Updated upstream
     <h1>inscription d'Adrien refusée</h1>
+=======
+    <h1>PAGE D'INSCRIPTION</h1>
+    <div class="inscription_container">
+        <form class="inscription" method="post" action ="model/traitement-inscription.php">
+>>>>>>> Stashed changes
 
-    <form method="post" action ="model/traitement-inscription.php">
-
-    <input type="text" >
-    <input type="password">
-    <input type="submit" value="s'inscrire">
+        <input class="placeholder" type="text" name="nom_d'utilisateur" placeholder="Entrer un nom d'utilisateur">
+        <input class="placeholder" type="password" name="password" placeholder="Créer le mot de passe" required="">
+        <input class="placeholder" type="password" name="confirm_password" placeholder="Confirmer le mot de passe" required="">
+        <input class="inscrire" type="submit" value="s'inscrire">
+    
 
 
     </form>
+</div>
 </body>
 </html>
