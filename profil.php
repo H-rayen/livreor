f09816f0ac8107702d2728ad65097543af1943a9
H-rayen <rayen.hasni@laplateforme.io>
2025-09-26 09:17:36 +0200

    edit profile

--- profil.php
+++ profil.php
@@ -1,8 +1,21 @@
 <?php
 //Une page permettant de modifier son profil
+// tableau pour définir les champs
+$user = [
+    "login" => "rayen",
+    "password" => "motdepasse"
+];
+
 ?>
 <?php include "structure/header.php"; ?>
 <body>
     <?php include "structure/menu.php"; ?>
+<form method="post" action="model/edit-profil.php">
+<label for="login">login :</label>
+<input type ="text" value="<?php echo $user["login"] ?> "id="login" name="login">
+<label for="password"></label>
+<input type ="password" value="<?php echo $user["password"]?>"id="password" name ="password">
+<input type ="submit" value="edit">
+</form>
 </body>
 </html>
