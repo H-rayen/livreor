d89e78864d607a3178bce5b8f5e7073d918d7d0d
H-rayen <rayen.hasni@laplateforme.io>
2025-09-29 16:55:08 +0200

    session_connexion

--- model/model_connexion.php
+++ model/model_connexion.php
@@ -1,12 +1,38 @@
-<?php 
+<?php
+session_start();
 include '../core/bdd.php';
-$login = $_POST['login'];
-$password = $_POST['password'];
-$id_session = 2;
+include '../core/tools.php';
 
-$query="SELECT utilisateurs 
-login = '$login', password = '$password' 
-WHERE id = '$id_session'";
-?>
+$stmt = $pdo->prepare("SELECT* FROM utilisateurs WHERE login =?");
 
- 
+
+//toujours ouvrir une session au début 
+if (!empty($_POST)) {
+    // il s'agit d'une condition qui dépend de l'existance des champs de connexion de l'utilisateur
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+    if (isset($_POST['login']) && isset($_POST['password'])) {
+
+        //on appelle les donnée enregristré depuis la bdd.sql user.
+        $stmt = $pdo->prepare("SELECT * FROM utilisateurs WHERE login =?");
+        $response = $stmt->execute([$login]);
+
+        $res = $stmt->fetch(PDO::FETCH_ASSOC);
+       
+    }
+
+    if ($res['password'] == $_POST['password']) {
+        $_SESSION['login'] = $res['login'];
+        header("Location:../connexion.php");
+        exit;
+    }
+}
+
+// verifier tel retour de variable 
+//      }
+//on vérifie id et password
+//on verifie si les informations correspondent au formulaire :{
+// si (oui){une session est crée
+// } 
+// si non {on renvoie dans un formulaire avec un message d'erreur optionel  
+//     }
